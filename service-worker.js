49f1c6f7ecdb4dbe483073b4f82d6a6e4c946f5d
Capshi <cap@cap.com>
2018-12-31 13:40:05 -0600

    Fix service worker things

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
 self.addEventListener("install", (event) => {
-    const indexPage = new Request("index.html");
+    const indexPage = new Request("./");
 
     event.waitUntil(
         fetch(indexPage).then(async (response) => {
             const cache = await caches.open("growplant-offline");
-            console.log(`Cached index page during Install ${response.url}`);
+            console.log(`[ServiceWorker] Cached index page during Install ${response.url}`);
 
             return cache.put(indexPage, response);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(`[ServiceWorker] Fetch ${event.request.url}`);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            console.log(`[ServiceWorker] Serving from cache ${event.request.url}`);
+            return response || fetch(event.request);
         })
     );
 });
